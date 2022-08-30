84ae72aef19bc104de4e96beda1df9cb1f1f7a79
fabiosimoesds <fabio.simoesds@gmail.com>
2022-08-30 16:41:06 +0100

    Users validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  has_many :messages
+  has_many :trips
+  has_many :invitations_as_asker, class_name: "invitation", foreign_key: :asker_id
+  has_many :invitations_as_receiver, class_name: "invitation", foreign_key: :receiver_id
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :dob, :address, presence: true
+  validates :last_name, format: { with: /\A[a-zA-Z]+/, message: "Use only letters and white space" }
+  validates :first_name, format: { with: /\A[a-zA-Z]+/, message: "Use only letters and white space" }
 end
