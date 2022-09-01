4d116e9aaec4b3e019d3fcab2b3f788e0909651c
lucasalles <lucasallesgomes@hotmail.com>
2022-09-01 15:51:22 -0300

    chatroom done, front-end need some changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,15 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      redirect_to chatroom_path(@chatroom)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: {message: @message})
+      )
+      head :ok
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
+    authorize @message
   end
 
   private
