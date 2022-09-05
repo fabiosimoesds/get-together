08c5b2ef869b3380fd9efa53fc01549866fcfac8
Rafael Goncalves <rafaelnity@yahoo.com.br>
2022-09-05 14:55:48 -0300

    navbar, banner, cards with js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -15,3 +15,9 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import NavbarController from "./navbar_controller"
+application.register("navbar", NavbarController)
+
+import TypedJsController from "./typed_js_controller"
+application.register("typed-js", TypedJsController)
