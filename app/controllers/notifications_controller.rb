7e0fd8ef3f04a41c3e586030fc7f9f04a83f9069
fabiosimoesds <fabio.simoesds@gmail.com>
2022-09-06 20:38:06 +0100

    Notification Cards are responsive and you can only see your notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = policy_scope(Notification)
+    @notifications = @notifications.where(recipient: current_user)
   end
 end
