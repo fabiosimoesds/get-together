90862a94fd1424255ce58ddd1bfeea3f1e31140c
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-06 02:02:39 -0300

    photo.purge debugged

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,9 +2,9 @@ class AlbumsController < ApplicationController
   before_action :set_album, only: %i[show edit update destroy]
 
   def index
-    @album = policy_scope(Album)
+    @albums = policy_scope(Album)
   end
-  
+
   def show
     authorize @album
   end
@@ -26,14 +26,17 @@ class AlbumsController < ApplicationController
     redirect_to user_path(user), status: :see_other
   end
 
-  # def delete_image_attachment
-  #   @album = Album.first
-  #   @user = current_user
-  #   @image = ActiveStorage::Blob.find_signed(params[:id])
-  #   raise
-  #   authorize @album
-  #   @image.purge
-  # end
+  def delete_photo_attachment
+    # @album = Album.find(params[:id])
+    ActiveStorage::Attachment.find(params[:id]).purge
+    # @album = @photo.record
+    # authorize @album
+    # @photo.purge
+    # redirect_to album_path(@photo.record), notice: 'Photo deleted'
+    # redirect_back(fallback_location: albums_path)
+    redirect_to action: 'index'
+    skip_authorization
+  end
 
   private
 
