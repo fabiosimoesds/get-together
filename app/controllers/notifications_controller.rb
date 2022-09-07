6cbae1d3acfdb0c5ab45423bedf5bed53e81ed1d
fabiosimoesds <fabio.simoesds@gmail.com>
2022-09-07 19:05:25 +0100

    Notification working and doesn`t disapear anymore

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,12 @@ class NotificationsController < ApplicationController
   def index
     @notifications = policy_scope(Notification)
     @notifications = @notifications.where(recipient: current_user)
+    @quantity = @notifications.where(read_at: nil).count
+    @notifications.each do |notification|
+      notification.read_at = DateTime.now
+      notification.save
+    end
   end
 end
+
+# Notification.where(recipient: current_user).where(read_at: nil).count
